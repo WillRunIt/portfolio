84f3ec80a744196eb87205ed875d0ab84951e116
Willem De Kerpel <dekerpelwillem@hotmail.com>
2024-06-19 13:33:16 +0300

    Add Base Views

--- main/views.py
+++ main/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return HttpResponse("<h2>Welcome to the home page</h2>")
+
+def portfolio(request):
+    return HttpResponse("<h2>Welcome to my portfolio</h2>")
+
+def about(request):
+    return HttpResponse("<h2>Welcome to the About me page</h2>")
+def contact(request):
+    return HttpResponse("<h2>Welcome to the Contact page</h2>")
